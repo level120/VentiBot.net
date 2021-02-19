acf05c6eec5153f5d4524191e0bacac240bf7a31
level120 <tnara9573@gmail.com>
2021-02-19 23:04:54 +0900

    fix: 불필요 내용 제거

--- VentiBot.net/VentiBot.cs
+++ VentiBot.net/VentiBot.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,17 +8,14 @@ using Telegram.Bot.Exceptions;
 using Telegram.Bot.Extensions.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
-using Telegram.Bot.Types.InlineQueryResults;
-using Telegram.Bot.Types.InputFiles;
 using Telegram.Bot.Types.ReplyMarkups;
 using File = System.IO.File;
 
 namespace VentiBot.net
 {
-    public sealed class VentiBot
+    public sealed partial class VentiBot
     {
         private const string DebugTokenPath = "./debug.token";
-
 
         private static TelegramBotClient _client;
         public static TelegramBotClient Client => _client ??= new TelegramBotClient(GetToken());
@@ -47,24 +43,17 @@ namespace VentiBot.net
 
             Console.WriteLine($"Start listening for @{me.Username}");
         }
+    }
 
-        #region Event
-
-        private static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    public partial class VentiBot
+    {
+        private static async Task HandleUpdateAsync(
+                ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
             var handler = update.Type switch
             {
                 UpdateType.Message => BotOnMessageReceived(update.Message),
                 UpdateType.EditedMessage => BotOnMessageReceived(update.Message),
-                UpdateType.CallbackQuery => BotOnCallbackQueryReceived(update.CallbackQuery),
-                UpdateType.InlineQuery => BotOnInlineQueryReceived(update.InlineQuery),
-                UpdateType.ChosenInlineResult => BotOnChosenInlineResultReceived(update.ChosenInlineResult),
-                // UpdateType.Unknown:
-                // UpdateType.ChannelPost:
-                // UpdateType.EditedChannelPost:
-                // UpdateType.ShippingQuery:
-                // UpdateType.PreCheckoutQuery:
-                // UpdateType.Poll:
                 _ => UnknownUpdateHandlerAsync(update)
             };
 
@@ -85,168 +74,54 @@ namespace VentiBot.net
 
             var action = message.Text.Split(' ').First() switch
             {
-                "/inline" => SendInlineKeyboard(message),
-                "/keyboard" => SendReplyKeyboard(message),
-                "/photo" => SendFile(message),
-                "/request" => RequestContactAndLocation(message),
-                _ => Usage(message)
+                "/list" => SendListMsgAsync(message),
+                _ => SendUsageMsgAsync(message)
             };
+
             await action;
-
-            // Send inline keyboard
-            // You can process responses in BotOnCallbackQueryReceived handler
-            static async Task SendInlineKeyboard(Message message)
-            {
-                await Client.SendChatActionAsync(message.Chat.Id, ChatAction.Typing);
-
-                // Simulate longer running task
-                await Task.Delay(500);
-
-                var inlineKeyboard = new InlineKeyboardMarkup(new[]
-                {
-                    // first row
-                    new []
-                    {
-                        InlineKeyboardButton.WithCallbackData("1.1", "11"),
-                        InlineKeyboardButton.WithCallbackData("1.2", "12"),
-                    },
-                    // second row
-                    new []
-                    {
-                        InlineKeyboardButton.WithCallbackData("2.1", "21"),
-                        InlineKeyboardButton.WithCallbackData("2.2", "22"),
-                    }
-                });
-                await Client.SendTextMessageAsync(
-                    chatId: message.Chat.Id,
-                    text: "Choose",
-                    replyMarkup: inlineKeyboard
-                );
-            }
-
-            static async Task SendReplyKeyboard(Message message)
-            {
-                var replyKeyboardMarkup = new ReplyKeyboardMarkup(new []
-                    {
-                        new KeyboardButton[] { "1.1", "1.2" },
-                        new KeyboardButton[] { "2.1", "2.2" },
-                    },
-                    resizeKeyboard: true
-                );
-
-                await Client.SendTextMessageAsync(
-                    chatId: message.Chat.Id,
-                    text: "Choose",
-                    replyMarkup: replyKeyboardMarkup
-
-                );
-            }
-
-            static async Task SendFile(Message message)
-            {
-                await Client.SendChatActionAsync(message.Chat.Id, ChatAction.UploadPhoto);
-
-                const string filePath = @"Files/tux.png";
-                await using var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
-                var fileName = filePath.Split(Path.DirectorySeparatorChar).Last();
-                await Client.SendPhotoAsync(
-                    chatId: message.Chat.Id,
-                    photo: new InputOnlineFile(fileStream, fileName),
-                    caption: "Nice Picture"
-                );
-            }
-
-            static async Task RequestContactAndLocation(Message message)
-            {
-                var requestReplyKeyboard = new ReplyKeyboardMarkup(new[]
-                {
-                    KeyboardButton.WithRequestLocation("Location"),
-                    KeyboardButton.WithRequestContact("Contact"),
-                });
-                await Client.SendTextMessageAsync(
-                    chatId: message.Chat.Id,
-                    text: "Who or Where are you?",
-                    replyMarkup: requestReplyKeyboard
-                );
-            }
-
-            static async Task Usage(Message message)
-            {
-                const string usage = "Usage:\n" +
-                                     "/inline   - send inline keyboard\n" +
-                                     "/keyboard - send custom keyboard\n" +
-                                     "/photo    - send a photo\n" +
-                                     "/request  - request location or contact";
-                await Client.SendTextMessageAsync(
-                    chatId: message.Chat.Id,
-                    text: usage,
-                    replyMarkup: new ReplyKeyboardRemove()
-                );
-            }
         }
 
-        // Process Inline Keyboard callback data
-        private static async Task BotOnCallbackQueryReceived(CallbackQuery callbackQuery)
+        private static async Task SendListMsgAsync(Message message)
         {
-            await Client.AnswerCallbackQueryAsync(
-                callbackQuery.Id,
-                $"Received {callbackQuery.Data}"
-            );
+            // todo: api 작업 후 list msg 확인
+            const string itemMsg = "Empty";
 
             await Client.SendTextMessageAsync(
-                callbackQuery.Message.Chat.Id,
-                $"Received {callbackQuery.Data}"
+                chatId: message.Chat.Id,
+                text: itemMsg,
+                replyMarkup: new ReplyKeyboardRemove()
             );
         }
 
-        #region Inline Mode
-
-        private static async Task BotOnInlineQueryReceived(InlineQuery inlineQuery)
+        private static async Task SendUsageMsgAsync(Message message)
         {
-            Console.WriteLine($"Received inline query from: {inlineQuery.From.Id}");
+            const string usage = "Usage:\n" +
+                                 "/list   - ??? 목록 확인";
 
-            InlineQueryResultBase[] results = {
-                // displayed result
-                new InlineQueryResultArticle(
-                    id: "3",
-                    title: "TgBots",
-                    inputMessageContent: new InputTextMessageContent(
-                        "hello"
-                    )
-                )
-            };
-
-            await Client.AnswerInlineQueryAsync(
-                inlineQuery.Id,
-                results,
-                isPersonal: true,
-                cacheTime: 0
+            await Client.SendTextMessageAsync(
+                chatId: message.Chat.Id,
+                text: usage,
+                replyMarkup: new ReplyKeyboardRemove()
             );
         }
-
-        private static async Task BotOnChosenInlineResultReceived(ChosenInlineResult chosenInlineResult)
-        {
-            Console.WriteLine($"Received inline result: {chosenInlineResult.ResultId}");
-        }
-
-        #endregion
 
         private static async Task UnknownUpdateHandlerAsync(Update update)
         {
             Console.WriteLine($"Unknown update type: {update.Type}");
         }
 
-        private static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        private static async Task HandleErrorAsync(
+            ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
         {
-            var ErrorMessage = exception switch
+            var errMsg = exception switch
             {
-                ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                ApiRequestException apiRequestException =>
+                    $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+
                 _ => exception.ToString()
             };
 
-            Console.WriteLine(ErrorMessage);
+            Console.WriteLine(errMsg);
         }
-
-        #endregion Event
     }
 }
